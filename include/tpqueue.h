7349d78f65cf4136f9ad6d04e3c875cdb8da136d
VilenskayaAlina <99177250+VilenskayaAlina@users.noreply.github.com>
2022-04-24 13:30:36 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,8 +4,43 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+  struct ITEM {
+    T data;
+    ITEM * next;
+  };
+ private:
+  TPQueue::ITEM* create(const T&);
+  ITEM *head; ITEM *tail;
+
+ public:
+  TPQueue():head(nullptr),tail(nullptr){}
+  ~TPQueue();
+  void push(const T&);
+  T pop();
+  void print() const;
+  
+  bool isEmpty() {
+    return head.isEmpty();
+  }
+  void push(const T&value) {
+    head.addHead(value);
+  }
+  T pop() {
+    if (!isEmpty) return head.rmHead;
+    else trow "Empty!";
+  }
+  T get() {
+    if (!isEmpty) return head.getHead;
+    else throw "Empty!";
 };
+
+template<typename T>
+  typename TPQueue<T>::ITEM* TPQueue<T>::create(const T& data) {
+    ITEM *item=new ITEM;
+    item−>data=data;
+    item−>next=nullptr;
+    return item;
+  }
 
 struct SYM {
   char ch;
