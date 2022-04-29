1f570a6e1d2334c3486cac50c3ee52dd072bdd64
VilenskayaAlina <99177250+VilenskayaAlina@users.noreply.github.com>
2022-04-29 22:22:44 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -33,11 +33,8 @@ class TPQueue {
           //prov1 = first->data.prior;
           //prov2 = temp->data.prior;
           //prov3 = first->next; //соответствие
-          if (((first->data.prior) == (temp->data.prior) &&
-               ((first->next) == nullptr || (first->data.prior) < (temp->data.prior))) ||
-              ((first->data.prior) > (temp->data.prior) &&
-               (((first->next) != nullptr && first->next->data.prior < (temp->data.prior))
-                || ((first->next) == nullptr)))) {
+          if (((first->data.prior) >= (temp->data.prior) &&
+        ((first->next) == nullptr || (first->data.prior) < (temp->data.prior)))) {
             temp->next = first->next;
             first->next = temp;
             return;
